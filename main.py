eb6613817ebc7ce6bf43e74be9e8e3fd7adf25db
mgmoldova <platonvadim@ya.ru>
2019-10-24 21:05:53 +0300

    Update main.py
    Clean up the code.

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ light_sensor = 0
 # D4 = servo
 ledbar = 4
 
+# D10 = potentiomentr
 threshold = 10
 
+# Setup pins
 grovepi.pinMode(light_sensor,"INPUT")
 grovepi.pinMode(ledbar,"OUTPUT")
+
+
+def normalize(x, in_min, in_max, out_min, out_max):
+    '''Normalize a number from one range to another.'''
+    return int((x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min)
 
 # main loop
 if __name__ == '__main__':
     while True:
         try:
             sensor_value = grovepi.analogRead(light_sensor)
-
-            resistance = (float)(1023 - sensor_value) * 10 / sensor_value
-
+            resistance = normalize(sensor_value, 0, 1023, 0, 10)
             if resistance < threshold:
                 grovepi.ledBar_setLevel(ledbar, int(resistance))
                 time.sleep(0.2)
